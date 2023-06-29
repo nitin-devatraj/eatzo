9b67860e011a8726a32ab73e4611552a5b85ce7c
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-30 00:23:16 +0530

    use useReducer to manage cart / context state in CartProvider component

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 function CartButton(props) {
+  const cartCtx = useContext(CartContext);
+  const totalCartItems = cartCtx.items.reduce((acculumator, item) => {
+    return acculumator + item.amount;
+  }, 0);
   return (
     <button onClick={props.onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{totalCartItems}</span>
     </button>
   );
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,38 @@
 import React from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (prevState, action) => {
+  if (action.type === "ADD") {
+    const updatedItems = prevState.items.concat(action.payload);
+    const updatedTotalAmount =
+      prevState.totalAmount + action.payload.price * action.payload.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  } else if (action.type === "REMOVE") {
+  } else {
+    return defaultCartState;
+  }
+};
+
 function CartProvider(props) {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", payload: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", payload: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
