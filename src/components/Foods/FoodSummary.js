67c79a9239386dbb124de2456fcdb4fd9f96364a
nitin devatraj <nitindevatraj@protonmail.com>
2023-06-30 22:24:48 +0530

    use useEffect & useState hooks to show cart animation on every cart action

--- src/components/Foods/FoodSummary.js
+++ src/components/Foods/FoodSummary.js
@@ -4,14 +4,14 @@ import classes from "./FoodSummary.module.css";
 function FoodSummary() {
   return (
     <section className={classes.summary}>
-      <h2>Remarkable culinary experience, right at your doorstep</h2>
+      <h2>Your Flavors, Delivered Fast</h2>
       <p>
-        Discover an extensive menu featuring delectable meals cooked by
-        experienced chefs using top-notch ingredients.
+        Craving a scrumptious meal from your favorite restaurants but don't want
+        to step out?
       </p>
       <p>
-        With convenient delivery, indulge in the pleasure of savoring delicious
-        food, brought to you with care and passion.
+        EatZo is here to tantalize your taste buds with a swift and convenient
+        food delivery experience.
       </p>
     </section>
   );
--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,15 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./CartButton.module.css";
 import CartContext from "../../store/cart-context";
 
 function CartButton(props) {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
   const cartCtx = useContext(CartContext);
-  const totalCartItems = cartCtx.items.reduce((acculumator, item) => {
+  const { items } = cartCtx;
+  const totalCartItems = items.reduce((acculumator, item) => {
     return acculumator + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnIsAnimated && classes.bump}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsAnimated(true);
+    const timerId = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
