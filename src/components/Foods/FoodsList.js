78a12f8bae25cf045a52e222059da8afa0a5d6d1
nitin devatraj <nitindevatraj@protonmail.com>
2023-07-09 01:33:22 +0530

    handle data loading and error state

--- src/components/Foods/FoodsList.js
+++ src/components/Foods/FoodsList.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import classes from "./FoodsList.module.css";
 import Card from "../UI/Card";
+import Spinner from "../UI/Spinner";
 import FoodItem from "./FoodItem/FoodItem";
 
 function FoodsList() {
   const [foodsList, setFoodsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchFoodsList = async () => {
       const response = await fetch(
         "https://eatzo-ac631-default-rtdb.europe-west1.firebasedatabase.app/foods.json"
       );
+
+      if (!response.ok) {
+        throw new Error("something went wrong, try again!");
+      }
+
       const data = await response.json();
       const loadedItems = [];
       for (const key in data) {
@@ -19,24 +27,43 @@ function FoodsList() {
         });
       }
       setFoodsList(loadedItems);
+      setIsLoading(false);
     };
-    fetchFoodsList();
+
+    fetchFoodsList().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
+
+  if (error) {
+    return (
+      <section className={classes.meals}>
+        <Card>
+          <p className={classes.error}>{error}</p>
+        </Card>
+      </section>
+    );
+  }
 
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {foodsList.map((item) => (
-            <FoodItem
-              key={item.id}
-              id={item.id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-            />
-          ))}
-        </ul>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {foodsList.map((item) => (
+              <FoodItem
+                key={item.id}
+                id={item.id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+              />
+            ))}
+          </ul>
+        )}
       </Card>
     </section>
   );
